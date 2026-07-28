0d9e6f09c4bbae499407516ba0b4bca04d55bb85
Prem Patel <rolexhere03@gmail.com>
2026-07-28 18:03:20 +0530

    --signup fixes

--- campuscircle-backend/src/api/auth.py
+++ campuscircle-backend/src/api/auth.py
@@ -112,8 +112,10 @@ async def signup(
     if not university:
         # Dynamic support for academic domains (.ac.in, .edu, .edu.in, .ac.uk, etc.)
         if any(email_domain.endswith(suffix) for suffix in [".edu", ".ac.in", ".edu.in", ".ac.uk", ".edu.au", ".in"]):
+            import uuid as uuid_lib
             uni_name = email_domain.split(".")[0].upper() + " University"
             university = University(
+                id=uuid_lib.uuid4(),
                 name=uni_name,
                 email_domain=email_domain
             )
@@ -147,6 +149,7 @@ async def signup(
     hashed_password = hash_password(payload.password)
     
     new_user = User(
+        id=uuid_lib.uuid4(),
         university_id=university.id,
         email=payload.email,
         username=payload.username,
